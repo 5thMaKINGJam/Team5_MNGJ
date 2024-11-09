6439ad1a51a442012d5773e5b4e391119dac03d1
Kminseo <childstone@users.noreply.github.com>
2024-11-09 15:22:07 +0900

    Player 다음 스테이지 연결 수정

--- MNGJ/Assets/Scripts/ButtonManager.cs
+++ MNGJ/Assets/Scripts/ButtonManager.cs
@@ -23,8 +23,24 @@ public class ButtonManager : MonoBehaviour
                 //선택화면으로 이동하기
                 break;
 
-            case "NextStage":
-                Debug.Log("다음 스테이지로");
+            case "GoSpring":
+                Debug.Log("봄 스테이지로");
+                //선택화면으로 이동하기
+                break;
+
+
+            case "GoSummer":
+                Debug.Log("여름 스테이지로");
+                //선택화면으로 이동하기
+                break;
+
+            case "GoFall":
+                Debug.Log("가을 스테이지로");
+                //선택화면으로 이동하기
+                break;
+
+            case "GoWinter":
+                Debug.Log("겨울 스테이지로");
                 //선택화면으로 이동하기
                 break;
 
