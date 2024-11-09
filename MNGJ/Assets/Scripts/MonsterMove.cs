28407ef7f46f1fc4a286edf26711b164d153fca1
sforseohn <sforseohn@ewhain.net>
2024-11-09 14:01:43 +0900

    feat: 원귀 이동방향에 따라 스프라이트 및 방향 변경

--- MNGJ/Assets/Scripts/MonsterMove.cs
+++ MNGJ/Assets/Scripts/MonsterMove.cs
@@ -10,8 +10,21 @@ public class MonsterMove : MonoBehaviour
     [SerializeField]
     private int speed;
 
+    [SerializeField]
+    private Sprite leftSprite;
+
+    [SerializeField]
+    private Sprite rightSprite;
+
+    [SerializeField]
+    private Sprite frontSprite;
+
+    [SerializeField]
+    private Sprite backSprite;
+
     private int curTarget = 0;
-    private int moveType; // 1: 직선 이동, 2: 코너 이동, 3: 네모 이동
+    private int moveType;
+    private SpriteRenderer spriteRenderer;
 
     private void Awake()
     {
@@ -21,24 +34,56 @@ public class MonsterMove : MonoBehaviour
     void Start()
     {
         moveType = points.Count;
+        spriteRenderer = GetComponent<SpriteRenderer>();
         this.transform.position = points[curTarget].transform.position;
     }
 
     void Update()
     {
-        
-        this.transform.position
-            = Vector3.MoveTowards(this.transform.position, points[curTarget].transform.position,
-        speed * Time.deltaTime);
+        Vector3 targetPosition = points[curTarget].transform.position;
 
+        this.transform.position
+            = Vector3.MoveTowards(transform.position, targetPosition, speed * Time.deltaTime);
+
+        Vector3 change = targetPosition - transform.position;
+        
+        ChangeRotation(change);
+
+        ChangeSprite(change);
+
+        ChangeTarget();
+    }
+    void ChangeRotation(Vector3 change)
+    {
+        float angle = Mathf.Atan2(change.y, change.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+    }
+
+    void ChangeSprite(Vector3 change)
+    {
+        if (Mathf.Abs(change.x) > Mathf.Abs(change.y))  // 좌우
+        {
+            if (change.x > 0)
+                spriteRenderer.sprite = rightSprite; 
+            else
+                spriteRenderer.sprite = leftSprite; 
+        }
+        else  // 상하
+        {
+            if (change.y > 0)
+                spriteRenderer.sprite = frontSprite; 
+            else
+                spriteRenderer.sprite = backSprite; 
+        }
+    }
+
+    void ChangeTarget()
+    {
         float distance = Vector3.Distance(transform.position, points[curTarget].transform.position);
 
         if (distance <= 0.1f)
         {
-            Debug.Log("reached to target");
-
             curTarget = (curTarget + 1) % moveType;
-            Debug.Log(curTarget);
         }
     }
 }
