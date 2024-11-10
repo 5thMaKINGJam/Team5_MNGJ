ed66afdc8b5a49a2edf62a086df66f2587dd46d7
Kminseo <childstone@users.noreply.github.com>
2024-11-10 09:15:36 +0900

    UI 팝업창 제외 디자인 추가

--- MNGJ/Assets/Scripts/PlayerUIControl.cs
+++ MNGJ/Assets/Scripts/PlayerUIControl.cs
@@ -8,9 +8,6 @@ public class PlayerUIControl : MonoBehaviour
     public Player player;
     public GameObject gameOverUI;
     public GameObject ClearUI;
-    public GameObject HpUI0;
-    public GameObject HpUI1;
-    public GameObject HpUI2;
     int Hp;
     bool achieveClearItem;
     string currentSceneName;
@@ -27,8 +24,6 @@ public class PlayerUIControl : MonoBehaviour
     {
         Hp=player.Hp;
         achieveClearItem=player.achieveClearItem;
-
-        ShowHp();
 
         if(Hp==0){
             ShowGameOverUI();
@@ -57,31 +52,5 @@ public class PlayerUIControl : MonoBehaviour
             GameManager.Instance.isClear[3] = true;
 
         ClearUI.SetActive(true);
-    }
-    
-    //Hp에 따라서 모양 달라짐
-    void ShowHp(){
-        switch(Hp){
-            case 3 :
-                HpUI0.SetActive(true);
-                HpUI1.SetActive(true);
-                HpUI2.SetActive(true);
-                break;
-            case 2 :
-                HpUI0.SetActive(true);
-                HpUI1.SetActive(true);
-                HpUI2.SetActive(false);
-                break;
-            case 1 :
-                HpUI0.SetActive(true);
-                HpUI1.SetActive(false);
-                HpUI2.SetActive(false);
-                break;
-            case 0 :
-                HpUI0.SetActive(false);
-                HpUI1.SetActive(false);
-                HpUI2.SetActive(false);
-                break;
-        }
     }
 }
